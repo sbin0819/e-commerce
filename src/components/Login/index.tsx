88b1840610d58fb4b4b9c5b316ff1562b22d4ef7
sbin0819 <sbinha123@gmail.com>
2021-06-30 19:23:43 +0900

    add: login markup

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -2,6 +2,9 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 // import { Input } from '../common';
 
+import { FcGoogle } from 'react-icons/fc';
+import { AiFillFacebook, AiFillGithub } from 'react-icons/ai';
+
 const Conatiner = styled.div`
   position: relative;
   margin-top: 120px;
@@ -17,9 +20,8 @@ const Logo = styled.div`
 `;
 
 const FormContainer = styled.form`
-  height: 600px;
   width: 300px;
-  padding: 40px 0;
+  padding: 35px 0 25px;
   display: flex;
   flex-direction: column;
   button {
@@ -51,6 +53,35 @@ const Button = styled.button`
   border-radius: 4px;
 `;
 
+const Config = styled.div`
+  gap: 15px;
+`;
+
+const SnsLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 300px;
+  padding: 35px 0 25px;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+  gap: 10px;
+  .info {
+    font-size: 14px;
+    color: #888;
+  }
+  .icons {
+    margin-top: 8px;
+    display: flex;
+    font-size: 45px;
+    gap: 15px;
+  }
+`;
+
+const NonMember = styled.div`
+  margin-top: 15px;
+  color: #888;
+`;
+
 const index = () => {
   return (
     <Conatiner>
@@ -62,9 +93,19 @@ const index = () => {
         <Input placeholder="비밀번호" type="password" />
         <Button>로그인</Button>
       </FormContainer>
-      <div></div>
-      <div></div>
-      <div></div>
+      <Config>
+        <Link to="/">비밀번호 재설정</Link>
+        <Link to="/">회원가입</Link>
+      </Config>
+      <SnsLogin>
+        <p className="info">SNS 계정으로 간편 로그인/회원가입</p>
+        <div className="icons">
+          <FcGoogle />
+          <AiFillFacebook />
+          <AiFillGithub />
+        </div>
+      </SnsLogin>
+      <NonMember>비회원 주문 조회하기</NonMember>
     </Conatiner>
   );
 };
