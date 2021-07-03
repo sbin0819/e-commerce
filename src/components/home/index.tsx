27de31feddd5a8b7b633a9b95b23fd8eab149bc5
sbin0819 <sbinha123@gmail.com>
2021-07-03 12:51:28 +0900

    add: shortcut makrup

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,36 +1,14 @@
 import styled from 'styled-components';
+import Header from './header';
+import Shortcut from './shortcut';
 
 const Container = styled.div``;
-
-const ImageBarnner = styled.div`
-  margin-top: 20px;
-  display: flex;
-  height: 400px;
-  .main {
-    flex: 2;
-    background: gold;
-    margin: 0 15px;
-  }
-  .event {
-    flex: 1;
-    background: pink;
-    margin: 0 15px;
-  }
-  @media (max-width: 650px) {
-    flex-direction: column;
-  }
-`;
-
-const ShortCut = styled.div``;
 
 const index = () => {
   return (
     <Container>
-      <ImageBarnner>
-        <div className="main">a</div>
-        <div className="event">b</div>
-      </ImageBarnner>
-      <ShortCut></ShortCut>
+      <Header />
+      <Shortcut />
     </Container>
   );
 };
