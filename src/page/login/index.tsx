0cddba06084de623d0c3516729c21a18b6b3bb9c
sbin0819 <sbinha123@gmail.com>
2021-07-04 18:54:18 +0900

    some chagend

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,7 +1,7 @@
-import Login from '../../components/Login';
+import Login from '../../components/Login'
 
 const index = () => {
-  return <Login />;
-};
+  return <Login />
+}
 
-export default index;
+export default index
