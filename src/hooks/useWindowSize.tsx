4244165d82eff127a18c6dd51c368813531cada4
sbin0819 <sbinha123@gmail.com>
2021-07-14 16:25:48 +0900

    some changed

--- src/hooks/useWindowSize.tsx
+++ src/hooks/useWindowSize.tsx
@@ -1,33 +1,30 @@
-import { useState, useLayoutEffect, useCallback } from 'react';
+import { useState, useLayoutEffect, useCallback } from 'react'
 
-/**
- * React hook for detecting window resizing
- */
 const useWindowSize = () => {
-  const isClient = typeof window === 'object';
+  const isClient = typeof window === 'object'
 
   const getSize = useCallback(() => {
     return {
       width: isClient ? window.innerWidth : undefined,
       height: isClient ? window.innerHeight : undefined,
-    };
-  }, [isClient]);
+    }
+  }, [isClient])
 
-  const [windowSize, setWindowSize] = useState(getSize);
+  const [windowSize, setWindowSize] = useState(getSize)
 
   useLayoutEffect((): any => {
     if (!isClient) {
-      return false;
+      return false
     }
     function handleResize() {
-      setWindowSize(getSize());
+      setWindowSize(getSize())
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, [getSize, isClient]);
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [getSize, isClient])
 
-  return windowSize;
-};
+  return windowSize
+}
 
-export default useWindowSize;
+export default useWindowSize
